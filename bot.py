c156616233fd948173bd7309c8da1d3ee2af2649
otabek05 <otabektoshmukhammedov@gmail.com>
2022-10-28 11:07:53 +0900

    uploading

---
+++ bot.py
@@ -0,0 +1,21 @@
+
+from aiogram import Bot, Dispatcher,executor
+from aiogram.types import Message
+
+TOKEN="5670030245:AAHB14ck5ramUTFzGlUIvB0QTzBLsJ5Ju-Y"
+
+bot =Bot(token=TOKEN)
+
+dp=Dispatcher(bot=bot)
+
+@dp.message_handler(commands="start")
+async def start(message:Message):
+    chat_id=message.from_user.id
+    await bot.send_message(
+    chat_id=chat_id,
+    text=f"Assalomu alaykum"
+)
+
+
+
+executor.start_polling(dispatcher=dp)
---
+++ weather.py
@@ -0,0 +1,7 @@
+import requests
+
+
+def get_weather_app():
+    response=requests.get(url="",params={
+        
+    })
